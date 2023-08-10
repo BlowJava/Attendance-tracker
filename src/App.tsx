70af5797ff98ec5ec424267cb47d96ba1d1348f3
trickrenzgarcia <trickrenzgarcia@gmail.com>
2023-08-11 06:50:14 +0800

    adsdx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
