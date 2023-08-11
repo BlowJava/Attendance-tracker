52d13254d236f170e19c545d75beafde6b1f196f
trickrenzgarcia <trickrenzgarcia@gmail.com>
2023-08-11 13:16:27 +0800

    rm

--- src/Data/Functions/CalculateTotalHours.ts
+++ src/Data/Functions/CalculateTotalHours.ts
@@ -1,9 +1,9 @@
-export const calculateTotalHours = (dailyData: []) => {
+export const calculateTotalHours = (dailyData: any) => {
   let totalHours = 0;
   for (const entry of dailyData) {
     if (entry.firstIn && entry.lastOut) {
-      const firstIn = new Date(entry.firstIn);
-      const lastOut = new Date(entry.lastOut);
+      const firstIn: any = new Date(entry.firstIn);
+      const lastOut: any = new Date(entry.lastOut);
       const hoursWorked = (lastOut - firstIn) / (1000 * 60 * 60); // Convert milliseconds to hours
       totalHours += hoursWorked;
     }
