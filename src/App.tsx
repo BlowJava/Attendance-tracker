9d3df77cab6d44f51a13a0ab1878a1d5453df732
trickrenzgarcia <trickrenzgarcia@gmail.com>
2023-08-11 10:51:27 +0800

    asdawdas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,76 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import Navbar from 'Components/Navbar'
+import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
+import mockJSON from './MOCK_DATA.json'
 
 function App() {
+
+  const data = useMemo(() => mockJSON, [])
+
+  /** @type import('@tanstack/react-table').ColumnDef<any> */
+  const columns = [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+      footer: 'ID'
+    },
+    {
+      header: 'First Name',
+      accessorKey: 'first',
+      footer: 'First Name'
+    },
+    {
+      header: 'Last Name',
+      accessorKey: 'last',
+      footer: 'Last Name'
+    },
+    {
+      header: 'Hours',
+      accessorKey: 'hours',
+      footer: 'Hours'
+    },
+    {
+      header: 'from',
+      accessorKey: 'start',
+      footer: 'from'
+    },
+    {
+      header: 'to',
+      accessorKey: 'end',
+      footer: 'to'
+    },
+  ]
+
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+  })
 
   return (
     <>
       <div className=''>
         <Navbar/>
+
+        <table>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => <th key={header.id}>
+                {flexRender(header.column.columnDef.header, header.getContext())}
+              </th>)}
+            </tr>
+          ))}
+        </table>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </div>
-      
     </>
   )
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
         <div className=''>
           <ul className='flex flex-row gap-4 text-lg'>
             <a href="#"><li className='rounded-sm  hover:bg-gray-200 ease-in duration-150 px-3 py-2'>Employee</li></a>
-            <a href="#"><li className='rounded-sm hover:bg-gray-200 ease-in duration-300 px-6 py-2'>OJT</li></a>
-            <a href="#"><li className='rounded-sm hover:bg-gray-200 ease-in duration-300 px-6 py-2'>Tite</li></a>
+            <a href="#"><li className='rounded-sm hover:bg-gray-200 ease-in duration-150 px-6 py-2'>OJT</li></a>
+            <a href="#"><li className='rounded-sm hover:bg-gray-200 ease-in duration-150 px-6 py-2'>Tite</li></a>
           </ul>
         </div>
       </div>
